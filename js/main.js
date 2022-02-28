80f24d62900aa5092004c9d2ae6b23be64ae0b0e
Bridgette <bridgettec@nait.ca>
2022-02-28 16:59:14 -0700

    updated demo copy with images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Step 1: select the element the user will click on to make this menu show/hide. In this case it's the toggle-icon and since we are grabbing it by it's classname we need to include the period 
-let clickButton = document.querySelector('.toggle-icon');
+let clickButton = document.querySelector('.toggle-btn');
 
 
 // Step 2: add a click event to that icon
